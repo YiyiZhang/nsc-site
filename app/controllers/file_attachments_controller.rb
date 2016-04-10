d3cc2a0fab02d8098a4f0bdf5c831e3ed4091b06
Henry Dotson <l3iodeez@gmail.com>
2016-04-10 19:21:46 -0400

    Create controllers.

---
+++ app/controllers/file_attachments_controller.rb
@@ -0,0 +1,21 @@
+class FileAttachmentsController < ApplicationController
+  before_action :set_file_attachment, only: [:destroy, :update]
+
+  def destroy
+    @file_attachment.destroy
+    if @file_attachment.attachable_type == 'StaticPage'
+      redirect_to edit_static_page_path(@file_attachment.attachable.idea)
+    end
+  end
+  def update
+  end
+  private
+  def set_file_attachment
+    @file_attachment = FileAttachment.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def file_attachment_params
+    params.require(:file_attachment).permit(:attachment, :attachment_name)
+  end
+end
